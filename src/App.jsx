25509ff7701ba663748bc7172c991d104ef1406b
福田 拓郎 <76578159+takurofukuta@users.noreply.github.com>
2021-07-04 23:49:41 +0900

    スイッチの切り替えで顔文字を表示する機能を実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessages from "./components/ColorfulMessages";
 
-const onClickButton = () => alert();
-
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [face, setFace] = useState(true);
+
+  const onClickButtonCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickButtonFace = () => {
+    setFace(!face);
+  };
+
   return (
     <>
       <ColorfulMessages color="blue" message="お元気ですか" />
       <ColorfulMessages color="pink" message="元気です!" />
-      <button onClick={onClickButton}>スイッチ</button>
+      <button onClick={onClickButtonCountUp}>カウントアップ</button>
+      <button onClick={onClickButtonFace}>on/off</button>
+      <p>{num}</p>
+      {face && <p> ( ^ω^ ) </p>}
     </>
   );
 };
